72de58b1b49c4c04edf7eef21c0f45aac02d8bfa
Fabrizio Carlassara <53102151+carlassaraf@users.noreply.github.com>
2022-06-22 15:16:51 -0300

    add fft example

---
+++ examples/misc/fft_hardcoded/src/fft_hardcoded.c
@@ -0,0 +1,20 @@
+/*
+===============================================================================
+ Name        : fft_hardcoded.c
+ Author      : $(author)
+ Version     :
+ Copyright   : $(copyright)
+ Description : main definition
+===============================================================================
+*/
+
+#include "chip.h"
+
+int main(void) {
+
+    SystemCoreClockUpdate();
+    while(1) {
+
+    }
+    return 0 ;
+}

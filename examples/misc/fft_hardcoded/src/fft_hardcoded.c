396979231ec89cfb71fca307882eb0208d218cc1
Fabrizio Carlassara <53102151+carlassaraf@users.noreply.github.com>
2022-07-19 12:59:14 -0300

    add hardcoded fft example

--- examples/misc/fft_hardcoded/src/fft_hardcoded.c
+++ examples/misc/fft_hardcoded/src/fft_hardcoded.c
@@ -9,12 +9,84 @@
 */
 
 #include "chip.h"
+#include "fft.h"
+#include <string.h>
+
+/* FFT Parameters and Variables */
+#define FFT_samples             64      //This value MUST ALWAYS be a power of 2
+#define FFT_samplingFrequency   200
+#define FFT_ampFactor           8.35f
+float vReal[FFT_samples];
+float vImag[FFT_samples];
+
+/* Simulated signal */
+// Sinusoidal Parameters and Variables
+#define sin_signals_number      5
+float sin_amplitude[sin_signals_number] = {10,     5,    17,    13,    20};
+float sin_frequency[sin_signals_number] = {10,      20,     65,     40,     83};
+
+// Signal Parameters and Variable
+#define signal_samples          FFT_samples
+#define signal_Offset           20000
+float signal[signal_samples];
+
+/* Auxiliar Functions */
+// Generate a signal from the sum of sinusoidal signals
+void SignalGenerator(float* signal_output, size_t signal_output_len, size_t signals_number, float* signals_amplitud, float* signals_frequency, uint16_t signal_offset, uint16_t sampling_frequency);
+// Remove the offset of a set of samples
+void RemoveOffset(float* array, size_t array_len);
 
 int main(void) {
+    /* FFT Initialization */
+    fft_init(vReal, vImag, FFT_samples, FFT_samplingFrequency);
+    /* Generating signal from values */
+    SignalGenerator(signal, signal_samples, sin_signals_number, sin_amplitude, sin_frequency, signal_Offset, FFT_samplingFrequency);
 
-    SystemCoreClockUpdate();
-    while(1) {
+    // Signal Conditioning
+    RemoveOffset(signal, signal_samples);
+    /* Copy values to memory */
+    memcpy(vReal, signal, sizeof(float)*signal_samples);
 
-    }
-    return 0 ;
+	fft_windowing(FFT_WIN_TYP_HAMMING, FFT_FORWARD); // Recommended use for high frequencies. Relative to Sampling Frequency. In this case: 70Hz
+	fft_compute(FFT_FORWARD);
+	fft_complex_to_magnitude();
+
+	float amp, freq;
+	fft_major_peak(&amp, &freq, FFT_ampFactor);
+
+    return 0;
 }
+
+void SignalGenerator(float* signal_output, size_t signal_output_len, size_t signals_number, float* signals_amplitud, float* signals_frequency, uint16_t signal_offset, uint16_t sampling_frequency)
+{
+    for (uint16_t i = 0; i < signals_number; i++)
+    {
+        float cycles = (((signal_output_len-1) * (float)signals_frequency[i]) / sampling_frequency); //Number of signal cycles that the sampling will read
+        for (uint16_t j = 0; j < signal_output_len; j++)
+        {
+            signal_output[j] += (float)((signals_amplitud[i] * (sin((j * (6.28318531 * cycles)) / signal_output_len))) / 2.0);/* Build data with positive and negative values*/
+        }
+    }
+
+    for (size_t i = 0; i < signal_output_len; i++)
+    {
+        signal_output[i] += signal_offset;
+    }
+}
+
+void RemoveOffset(float* array, size_t array_len)
+{
+	// calculate the mean of vData
+	float mean = 0;
+	for (size_t i = 0; i < array_len; i++)
+	{
+		mean += array[i];
+	}
+	mean /= array_len;
+	// Subtract the mean from vData
+	for (size_t i = 0; i < array_len; i++)
+	{
+		array[i] -= mean;
+	}
+}
+
